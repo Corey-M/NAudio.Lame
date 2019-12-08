abd8ee617ad2adb6fe78c0b34b1da5cc21933cee
Corey Murtagh <Corey.Murtagh@mercuryit.com.au>
2019-12-08 22:55:06 +1000

    Updated Lame.Test.Core to Core2.1, added NetFramework test project.

---
+++ Lame.Test.Framework/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lame.Test.Framework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lame.Test.Framework")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("df68a50e-1072-4c6b-89f7-6b7ccdb7ae5d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
